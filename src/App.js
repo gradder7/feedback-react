d5882d07234cb385f94ec90d80346e8274957e26
Himanshu <mehra3228@gmail.com>
2023-01-27 11:48:34 +0530

    Code reseted and clear last 3-4 commit

--- src/App.js
+++ src/App.js
@@ -7,18 +7,21 @@ import FeedbackLists from "./Components/FeedbackLists";
 import FeedbackStats from "./Components/FeedbackStats";
 import Header from "./Components/Header";
 import FeedbackData from "./data/FeedbackData";
-import { BrowserRouter as Router, Routes, Route ,NavLink} from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  NavLink,
+} from "react-router-dom";
 import About from "./pages/About";
 import AbouticonLink from "./Components/AbouticonLink";
-import Card from "./Components/shared/Card";
-import Post from "./Components/Post";
 
 function App() {
   // feedback data is an array in data folder
   const [feedBack, setFeedBack] = useState(FeedbackData);
 
   // we have to provide the id to our feedback items
-  // npm install uuid 
+  // npm install uuid
   //to generate a unique id for each of out feedback items
   const addFeedback = (newFeedback) => {
     // add id to newFeedback
@@ -40,34 +43,24 @@ function App() {
         <Header text={"Header-Feedback"} />
         <div className="container">
           <Routes>
-            <Route exact path="/" element={
-              <>
-              <FeedbackForm handleAdd={addFeedback} />
-              <FeedbackStats feedback={feedBack} />
-              <FeedbackLists
-                feedback={feedBack}
-                // prop-Drilling(passing function as a prop to the child level where on click the function call will happen and it will call the function throughs the prop till it not executes )
-                handleDelete={deleteFeedBack}
-              />
-              </>
-            }>
-            </Route>
+            <Route
+              exact
+              path="/"
+              element={
+                <>
+                  <FeedbackForm handleAdd={addFeedback} />
+                  <FeedbackStats feedback={feedBack} />
+                  <FeedbackLists
+                    feedback={feedBack}
+                    // prop-Drilling(passing function as a prop to the child level where on click the function call will happen and it will call the function throughs the prop till it not executes )
+                    handleDelete={deleteFeedBack}
+                  />
+                </>
+              }
+            ></Route>
             <Route exact path="/about" element={<About />} />
-            {/* /post/:id id is params which we will recive in componenet as a object from useParams*/}
-            {/* /post/* beacause it may have nested routes */}
-            <Route exact path="/post/*" element={<Post />} />
           </Routes>
-
-          {/* nav links are used to chnage the classes when clicked on link */}
-          <Card>
-            <NavLink to='/' activeClassName='active'>
-              Home
-            </NavLink>
-            <NavLink to='/about' activeClassName='active'>
-              About
-            </NavLink>
-          </Card>
-          <AbouticonLink/>
+          <AbouticonLink />
         </div>
       </Router>
     </>
